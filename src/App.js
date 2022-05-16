8117427214f6c2469ab150e375d658a1087c8e4e
anyasya <anyasya1999@gmail.com>
2022-05-16 23:21:52 +0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Task1 from "./tasks/task1/task1";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>
-          Fun facts about React
-        </h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li> Was originally created by Jordan Walke</li>
-          <li> Has well over 100K stars on GitHub</li>
-          <li> Is maintained by Facebook</li>
-          <li> Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-        <div>
-
-        </div>
+        <span>React Course - Project 1</span>
       </header>
+        <Task1/>
     </div>
   );
 }
 
 export default App;
+
+
+
--- src/index.js
+++ src/index.js
@@ -5,32 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-const test = (
-    <div>
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li> Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li> Is maintained by Facebook</li>
-            <li> Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-
-document.getElementById('root').append(test)
-
-
-//
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-//
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
