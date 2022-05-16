1cccf642a0db2f5d480f5b5f08759347ddb7ecce
anyasya <anyasya1999@gmail.com>
2022-05-16 21:28:25 +0300

    init project

--- src/index.js
+++ src/index.js
@@ -4,14 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const test = (
+    <div>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li> Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li> Is maintained by Facebook</li>
+            <li> Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+
+document.getElementById('root').append(test)
+
+
+//
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+//
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
